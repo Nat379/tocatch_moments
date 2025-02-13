f85630d6d85c52878d605c961e485122e8ef2b17
Natalia Shkalikova <nataliashkalikova@MacBook-Air-Natalia.local>
2025-02-13 22:45:48 +0200

    add portfolio- and services-section

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+   // Ініціалізація слайдера мініатюр
+  const galleryThumbs = new Swiper('.gallery-thumbs', {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    freeMode: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: '.swiper-button-next', // Прив'язка кнопок до нижнього слайдера
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  // Ініціалізація основного слайдера
+  const galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true, // Включений цикл для головного слайдера
+    navigation: {
+      nextEl: '.swiper-button-next', // Прив'язка кнопок до верхнього слайдера
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs, // Прив'язка до мініатюр
+    },
+  });
+  
+  
+})
+
+
+
+
 // const scrollContainer = document.querySelector('.scroll-container');
 // const scrollContent = document.querySelector('.scroll-content');
 
