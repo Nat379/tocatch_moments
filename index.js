c7ccff42dbd5478a8ff67b624408a039587d804d
Natalia Shkalikova <nataliashkalikova@MacBook-Air-Natalia.local>
2025-02-27 22:42:06 +0200

    fix horizontal scroll on mobile version

--- index.js
+++ index.js
@@ -26,4 +26,5 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
   document.getElementById("year").textContent = new Date().getFullYear();
+  document.getElementById("mobile-year").textContent = new Date().getFullYear();
 })
