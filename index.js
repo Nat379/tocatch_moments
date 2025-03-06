9bed1e42d2924a8edcb3f905cc19e49aa2b48540
Natalia Shkalikova <nataliashkalikova@MacBook-Air-Natalia.local>
2025-03-06 23:31:51 +0200

    add burger-menu

--- index.js
+++ index.js
@@ -10,33 +10,56 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-
+  
   document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
+
+  const burger = document.querySelector(".burger-menu");
+  const menu = document.querySelector(".menu");
+  const overlay = document.querySelector(".overlay");
+  const body = document.body;
+
+  // Функція для відкриття/закриття меню
+  function toggleMenu() {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+    body.classList.toggle("lock"); // Заборона прокручування
+  }
+
+  // Відкриття/закриття при кліку на бургер
+  burger.addEventListener("click", toggleMenu);
+
+
+  // Закриття меню при кліку на посилання
+  menu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", toggleMenu);
+  });
+
+
   // Ініціалізація слайдера мініатюр
-  // const galleryThumbs = new Swiper('.gallery-thumbs', {
-  //   loop: true,
-  //   spaceBetween: 10,
-  //   slidesPerView: 'auto',
-  //   freeMode: true,
-  //   watchSlidesProgress: true,
-  //   navigation: {
-  //     nextEl: '.swiper-button-next', // Прив'язка кнопок до нижнього слайдера
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  // });
+  const galleryThumbs = new Swiper('.gallery-thumbs', {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    freeMode: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: '.swiper-button-next', // Прив'язка кнопок до нижнього слайдера
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
   // Ініціалізація основного слайдера
-  const galleryTop = new Swiper(".gallery-top", {
-    spaceBetween: 10,
-    loop: true, // Включений цикл для головного слайдера
-    navigation: {
-      nextEl: ".swiper-button-next", // Прив'язка кнопок до верхнього слайдера
-      prevEl: ".swiper-button-prev",
-    },
+  // const galleryTop = new Swiper(".gallery-top", {
+  //   spaceBetween: 10,
+  //   loop: true, // Включений цикл для головного слайдера
+  //   navigation: {
+  //     nextEl: ".swiper-button-next", // Прив'язка кнопок до верхнього слайдера
+  //     prevEl: ".swiper-button-prev",
+  //   },
     // thumbs: {
     //   swiper: galleryThumbs, // Прив'язка до мініатюр
     // },
-  });
+  // });
 
   backToTopButton.addEventListener("click", () => {
     window.scrollTo({
